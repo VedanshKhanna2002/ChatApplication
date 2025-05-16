1a1837b1e28199b0e3d172a18f0e445dfa44439e
VedanshKhanna2002 <115338791+VedanshKhanna2002@users.noreply.github.com>
2025-05-17 01:28:27 +0530

    Added Home Screen and Contact List Displayed on the Screen and now working on bottom navigation bar

--- app/src/main/java/com/vedansh/whatsapp/presentation/bottomnavigation/BottomNavigation.kt
+++ app/src/main/java/com/vedansh/whatsapp/presentation/bottomnavigation/BottomNavigation.kt
@@ -1,2 +1,10 @@
 package com.vedansh.whatsapp.presentation.bottomnavigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun bottomNvaigation(){
+
+}
